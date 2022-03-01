cbd969b0e49505d6d34ca34be2a5ccda1f07217c
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2022-03-01 18:48:59 +0200

    Adds tailwind (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+import "@/assets/index.css";
 </script>
 
 <template>
