18349431e51bd4d2f7d14f3329faf80f5408e8e7
Dusan Turajlic <dusan@turajlic.fi>
2022-03-01 23:13:11 +0200

    Add support for google Login

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+/// <reference types="node" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_GOOGLE_API_KEY: string;
+  readonly VITE_APP_GOOGLE_CLIENT_ID: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
