cdb5776544f2fd597f0580f1360b5f4453f8d269
Dusan Turajlic <dusan@turajlic.fi>
2022-03-05 22:53:16 +0200

    Made Login page

---
+++ src/components/InfoSection.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = defineProps<{ title: string }>();
+</script>
+<template>
+  <div class="relative">
+    <dt>
+      <div
+        class="absolute h-12 w-12 flex items-center justify-center border rounded-md"
+      >
+        <slot name="icon" />
+      </div>
+      <p class="ml-16 text-lg leading-6 font-medium">{{ title }}</p>
+    </dt>
+    <dd class="mt-2 ml-16 text-base text-gray-500">
+      <slot />
+    </dd>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00BD7E",
+        secondary: "",
+      },
+    },
   },
   plugins: [],
 };
