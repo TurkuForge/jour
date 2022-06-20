51076f45035af7870b6964f3fc591bae1532bf86
Dusan Turajlic <45139784+dusan-turajlic@users.noreply.github.com>
2022-06-20 08:28:21 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === "production" ? "/Jour/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/jour/" : "/",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
